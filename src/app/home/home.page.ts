4c4089bd4c5eda4b99b931f24886ddad17d02479
Yesid Vanegas <yesidvanegas68@gmail.com>
2024-06-23 19:26:32 -0500

    Verificacion de conexion a internet hecha

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Network } from '@capacitor/network';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
-  constructor() {}
+  constructor(
+    private navCtrl: NavController,
+    private toastCtrl: ToastController
+  ) {}
+
+  ngOnInit() {
+    this.checkConnectionAndNavigate();
+  }
+
+  async checkConnectionAndNavigate() {
+    const status = await Network.getStatus();
+
+    if (status.connected) {
+      // Si hay conexión, navega a la siguiente página
+      this.navCtrl.navigateForward('/ingreso');
+    } else {
+      // Si no hay conexión, muestra un mensaje de error
+      const toast = await this.toastCtrl.create({
+        message: 'No hay conexión a internet. Por favor, verifica tu conexión e intenta nuevamente.',
+        duration: 5000,
+        position: 'bottom'
+      });
+      toast.present();
+    }
+  }
 }
